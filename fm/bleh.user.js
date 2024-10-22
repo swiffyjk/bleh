a2813b5ddc835f4a1490edfc80dadf313a66d219
kate <46572320+katelyynn@users.noreply.github.com>
2024-10-22 03:29:00 +0100

    fix: some buttons losing colouring recently

--- fm/bleh.user.js
+++ fm/bleh.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         bleh
 // @namespace    http://last.fm/
-// @version      2024.1020
+// @version      2024.1021
 // @description  bleh!!! ^-^
 // @author       kate
 // @match        https://www.last.fm/*
@@ -18,7 +18,7 @@
 // ==/UserScript==
 
 let version = {
-    build: '2024.1020',
+    build: '2024.1021',
     sku: 'falter',
     feature_flags: {
         bleh_settings_tabs: {
