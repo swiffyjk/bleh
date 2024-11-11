1f0c010ce6f6f8910b2e76627ca034c35fa10cf8
kate <46572320+katelyynn@users.noreply.github.com>
2024-11-11 12:17:35 +0000

    fix: lang error sad crash

--- fm/bleh.user.js
+++ fm/bleh.user.js
@@ -2087,7 +2087,7 @@ function lookup_lang() {
     non_override_lang = lang;
 
     if (!valid_langs.includes(lang)) {
-        log(`language fallback from ${lang} to ${en} - not supported`, 'trans');
+        log(`language fallback from ${lang} to en - not supported`, 'trans');
         lang = 'en';
     }
 
