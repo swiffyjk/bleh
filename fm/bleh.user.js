60b6422d8e60ef74b5461b3f508ffc6d62d5717e
kate <46572320+katelyynn@users.noreply.github.com>
2024-12-13 23:48:21 +0000

    fix: a cappella

--- fm/bleh.user.js
+++ fm/bleh.user.js
@@ -2806,7 +2806,7 @@ let includes = {
         '[v1', '[v2', '[v3', '[v4', '[v5', '[v6', '[v7', '[v8', '[v9'
     ],
     stems: [
-        '- acapella', '(acapella', '[acapella',
+        '- acapella', '(acapella', '[acapella', '- a cappella', '(a cappella', '[a cappella',
         '- instrumental', '(instrumental', '[instrumental',
         '- session', '(session', '[session',
         '- studio session', '(studio session', '[studio session',
