9119d1f1fde9882c5172c2c8ebdd24c015506b02
kate <46572320+katelyynn@users.noreply.github.com>
2024-12-28 20:14:50 +0000

    feat: add global function to load and parse badges

--- fm/bleh.user.js
+++ fm/bleh.user.js
@@ -18292,4 +18292,46 @@ let has_prompted_for_update = false;
             });
         }
     }
+
+
+
+
+    function load_badges(user, solo = false) {
+        if (sponsor_list == null)
+            return;
+
+        if (!sponsor_list.badges.hasOwnProperty(user))
+            return;
+
+        let badges = [];
+
+        if (!Array.isArray(sponsor_list.badges[user])) {
+            log('1 badge found', 'sponsor', 'info', sponsor_list.badges[user]);
+            badges.push(sponsor_list.badges[user]);
+        } else {
+            log('multiple badges found', 'sponsor', 'info', sponsor_list.badges[user]);
+
+            if (solo)
+                badges.push(sponsor_list.badges[user][0])
+            else
+                badges = sponsor_list.badges[user];
+        }
+
+        // now we run thru to add missing metadata
+        badges.forEach((badge) => {
+            if (!badge.name)
+                badge.name = trans[lang].badges[badge.type];
+
+            if (badge.reason)
+                return;
+
+            if (badge.type == 'sponsor' || badge.type == 'contributor' || badge.type == 'translation')
+                badge.reason = badge.type;
+            else
+                badge.reason = 'reserved';
+        });
+
+        log('final badge list', 'sponsor', 'info', badges);
+        return badges;
+    }
 })();
