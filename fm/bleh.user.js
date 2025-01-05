e475e3065f714a5ed0dcb435a06fcf379f89e388
kate <46572320+katelyynn@users.noreply.github.com>
2025-01-05 20:57:49 +0000

    fix: gallery sort being missing

--- fm/bleh.user.js
+++ fm/bleh.user.js
@@ -12520,6 +12520,18 @@ let has_prompted_for_update = false;
         page.structure.main.after(bookmarks_content);
 
 
+        let sort_button = page.structure.main.querySelector('.dropdown-menu-clickable-button');
+        let sort_menu = page.structure.main.querySelector('.dropdown-menu-clickable');
+
+        let sort_wrap = document.createElement('div');
+        sort_wrap.classList.add('dropdown-top-wrap');
+
+        sort_wrap.appendChild(sort_button);
+        sort_wrap.appendChild(sort_menu);
+
+        page.structure.main.insertBefore(sort_wrap, page.structure.main.firstElementChild);
+
+
         // append images
         if (bookmarked_images.hasOwnProperty(page.name)) {
             bookmarked_images[page.name].forEach((image) => {
