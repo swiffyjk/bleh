775f27a031517c88a17871d8f89fb4b0717464a8
kate <46572320+katelyynn@users.noreply.github.com>
2025-01-18 22:11:26 +0000

    chore: bump ver

--- fm/bleh.user.js
+++ fm/bleh.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         bleh
 // @namespace    http://last.fm/
-// @version      2025.0114
+// @version      2025.0118
 // @description  bleh!!! ^-^
 // @author       kate
 // @match        https://www.last.fm/*
@@ -20,7 +20,7 @@
 
 let version = {
     brand: 'bleh',
-    build: '2025.0114.1',
+    build: '2025.0118',
     sku: 'mita',
     feature_flags: {
         bleh_settings_tabs: {
