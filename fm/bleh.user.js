fd218695685a74b061b00d3458c66360e6304526
katelyn <46572320+katelyynn@users.noreply.github.com>
2025-02-01 00:54:00 +0000

    feat: roll out update to fix notifs ahead of tomorrow

--- fm/bleh.user.js
+++ fm/bleh.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         bleh
 // @namespace    http://last.fm/
-// @version      2025.0125
+// @version      2025.0201
 // @description  bleh!!! ^-^
 // @author       kate
 // @match        https://www.last.fm/*
@@ -20,7 +20,7 @@
 
 let version = {
     brand: 'bleh',
-    build: '2025.0125',
+    build: '2025.0201',
     sku: '4x4',
     feature_flags: {
         bleh_settings_tabs: {
