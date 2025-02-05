c760abfbf82c4a3bcf6f078e8a28c3252b596d9e
katelyn <46572320+katelyynn@users.noreply.github.com>
2025-02-05 16:56:53 +0000

    fix: nag and notifications

--- fm/src/components/nag_bar.js
+++ fm/src/components/nag_bar.js
@@ -7,23 +7,22 @@ function nag_bar() {
             return;
     }*/
 
-    let active_nag = document.body.querySelector('.nag-bar');
-    if (!active_nag)
-        return;
+    let nags = page.structure.wrapper.querySelectorAll('.nag-bar');
+    nags.forEach((active_nag) => {
+        let type = active_nag.classList[1];
 
-    let type = active_nag.classList[1];
+        if (type == 'nag-bar--corrections') {
+            notify({
+                id: 'corrections',
+                title: trans[lang].nag_bar.corrections.title,
+                body: active_nag.querySelector('strong').innerHTML,
+                icon: 'icon-16-refresh'
+            });
+        } else {
+            // TODO
+            return;
+        }
 
-    if (type == 'nag-bar--corrections') {
-        notify({
-            id: 'corrections',
-            title: trans[lang].nag_bar.corrections.title,
-            body: active_nag.querySelector('strong').innerHTML,
-            icon: 'icon-16-refresh'
-        });
-    } else {
-        // TODO
-        return;
-    }
-
-    active_nag.parentElement.removeChild(active_nag);
+        active_nag.parentElement.removeChild(active_nag);
+    });
 }
