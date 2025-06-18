8a75eb86e269e70b78322c0869d2ea4e4ca3ad83
katelyn <46572320+katelyynn@users.noreply.github.com>
2025-06-18 14:38:09 +0100

    feat: select functionality

--- fm/bleh.user.js
+++ fm/bleh.user.js
@@ -16790,39 +16790,98 @@
   // src/components/rabbit.js
   function register_rabbit() {
     page.state.cmd = false;
+    let input_box;
+    let selected = 0;
+    let feed = 0;
+    let rabbit_hole;
     document.addEventListener("keydown", (e) => {
       if (e.ctrlKey || e.metaKey)
         page.state.cmd = true;
       if (page.state.cmd && e.key == "," && !page.structure.dialogs.hasChildNodes()) {
         rabbit();
-      } else if (e.key == "Escape" && page.structure.dialogs.hasChildNodes() && page.structure.dialogs.querySelector(':scope > [data-modal-type="rabbit"]')) {
-        dialog_rm2({ id: "rabbit" });
+      } else if (page.structure.dialogs.hasChildNodes() && page.structure.dialogs.querySelector(':scope > [data-modal-type="rabbit"]')) {
+        if (e.key == "Escape")
+          dialog_rm2({ id: "rabbit" });
+        if (e.key == "Tab") {
+          e.preventDefault();
+          rabbit_tab();
+        }
+        if (e.key == "ArrowDown") {
+          e.preventDefault();
+          if (selected < feed.length - 1)
+            selected++;
+          else
+            selected = 0;
+          rabbit_select();
+        } else if (e.key == "ArrowUp") {
+          e.preventDefault();
+          if (selected > 0)
+            selected--;
+          else
+            selected = feed.length - 1;
+          rabbit_select();
+        } else if (e.key != "ArrowLeft" && e.key != "ArrowRight") {
+          rabbit_search();
+        }
       }
     });
     document.addEventListener("keyup", (e) => {
       if (e.ctrlKey || e.metaKey)
         page.state.cmd = false;
     });
-  }
-  function rabbit() {
-    let input_box;
-    page.state.rabbit_modal = dialog({
-      id: "rabbit",
-      title: "rabbit",
-      body: html.node`
-        ${() => {
-        input_box = input({
-          maxlength: 100,
-          placeholder: tl(trans.anything_you_can_imagine),
-          focus: true
-        });
-        return input_box;
-      }}
-        `,
-      type: "rabbit",
-      replace_if_possible: true
-    });
-    input_box.querySelector("input").focus();
+    function rabbit() {
+      page.state.rabbit_modal = dialog({
+        id: "rabbit",
+        title: "rabbit",
+        body: html.node`
+                ${() => {
+          input_box = input({
+            maxlength: 100,
+            placeholder: tl(trans.anything_you_can_imagine),
+            focus: true
+          });
+          return input_box;
+        }}
+                <div class="rabbit-hole" ref=${(el) => rabbit_hole = el} />
+            `,
+        type: "rabbit",
+        replace_if_possible: true
+      });
+      rabbit_search();
+      rabbit_select();
+      input_box.querySelector("input").focus();
+    }
+    function rabbit_tab() {
+      input_box.querySelector("input").focus();
+    }
+    function rabbit_search() {
+      selected = 0;
+      feed = [0, 1, 2, 3, 4, 5, 6, 7];
+      render(rabbit_hole, html`
+            <button class="dropdown-menu-clickable-item rabbit-hole-item">0</button>
+            <button class="dropdown-menu-clickable-item rabbit-hole-item">1</button>
+            <button class="dropdown-menu-clickable-item rabbit-hole-item">2</button>
+            <button class="dropdown-menu-clickable-item rabbit-hole-item">3</button>
+            <button class="dropdown-menu-clickable-item rabbit-hole-item">4</button>
+            <button class="dropdown-menu-clickable-item rabbit-hole-item">5</button>
+            <button class="dropdown-menu-clickable-item rabbit-hole-item">6</button>
+            <button class="dropdown-menu-clickable-item rabbit-hole-item">7</button>
+        `);
+      rabbit_select();
+    }
+    function rabbit_select() {
+      notify({
+        id: "rabbit",
+        title: selected
+      });
+      let buttons = rabbit_hole.querySelectorAll("button");
+      buttons.forEach((button, index) => {
+        if (index == selected)
+          button.setAttribute("aria-checked", "true");
+        else
+          button.setAttribute("aria-checked", "false");
+      });
+    }
   }
 
   // src/page.js
