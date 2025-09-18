c60b04804a717d45b4c9b51bd75f0c239b7ada93
katelyn <46572320+katelyynn@users.noreply.github.com>
2025-09-18 19:25:50 +0100

    fix: solarium

--- fm/src/page.js
+++ fm/src/page.js
@@ -189,8 +189,6 @@ function bleh_main() {
 }
 
 function solarium() {
-    if (!settings.solarium) return;
-
     document.body.appendChild(html.node`
         <svg style="position: absolute; width: 0; height: 0">
             <filter id="solarium" x="-50%" y="-50%" width="200%" height="200%" primitiveUnits="objectBoundingBox">
